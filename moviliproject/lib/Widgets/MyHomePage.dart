5e3e337b09f8028e80b9245d6610cb94bf6054fb
joslex9 <134178860+joslex9@users.noreply.github.com>
2024-04-14 14:58:11 -0500

    my app myhome y main

---
+++ moviliproject/lib/Widgets/MyHomePage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [],
+        ),
+      ),
+    );
+  }
+}
